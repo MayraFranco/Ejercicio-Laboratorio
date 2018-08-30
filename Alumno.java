cdbb64b53071b65aaaca1240de23457b1f853abe
MayraFranco <mayra141220@gmail.com>
2018-08-30 10:01:16 -0500

    Intercambio inicial de proyecto

---
+++ Alumno.java
@@ -0,0 +1,11 @@
+public class Alumno
+{ 
+    private String nombre;
+    private int clave;
+    
+    public void alumno(String elNombre,int laClave)
+    {
+        nombre = elNombre;
+        clave = laClave;
+    }
+}
---
+++ Laboratorio.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Laboratorio
+{
+    private ArrayList<Alumno>alumnos;
+    
+    public Laboratorio ()
+    {
+        alumnos = new ArrayList<Alumno>();
+    }
+    public void inscribir()
+    {
+        System.out.println("Alta de alumnos");
+        for(int i = 0; i < alumnos.size(); i++)
+        {
+            
+        }
+    }
+    
+    
+}
